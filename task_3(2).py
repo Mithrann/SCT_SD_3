ec04e83be8560f957626ab2efb4c694dfd3c1f62
Mithrann <adulapurammithran@gmail.com>
2025-01-08 16:16:15 +0530

    Add files via upload

---
+++ task_3(2).py
@@ -0,0 +1,62 @@
+def print_board(board):
+    for row in board:
+        print(" ".join(str(num) if num != 0 else '.' for num in row))
+
+def is_valid(board, row, col, num):
+    # Check the row and column
+    for i in range(9):
+        if board[row][i] == num or board[i][col] == num:
+            return False
+    
+    # Check the 3x3 subgrid
+    start_row, start_col = 3 * (row // 3), 3 * (col // 3)
+    for i in range(start_row, start_row + 3):
+        for j in range(start_col, start_col + 3):
+            if board[i][j] == num:
+                return False
+    
+    return True
+
+def find_empty_location(board):
+    for i in range(9):
+        for j in range(9):
+            if board[i][j] == 0:
+                return (i, j)
+    return None
+
+def solve_sudoku(board):
+    empty = find_empty_location(board)
+    if not empty:
+        return True  # Puzzle solved
+    
+    row, col = empty
+    for num in range(1, 10):
+        if is_valid(board, row, col, num):
+            board[row][col] = num
+            if solve_sudoku(board):
+                return True
+            board[row][col] = 0  # Undo move (backtrack)
+    
+    return False
+
+# Example input grid (0 represents empty cells)
+sudoku_grid = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]
+]
+
+print("Unsolved Sudoku:")
+print_board(sudoku_grid)
+
+if solve_sudoku(sudoku_grid):
+    print("\nSolved Sudoku:")
+    print_board(sudoku_grid)
+else:
+    print("No solution exists for the given Sudoku puzzle.")
